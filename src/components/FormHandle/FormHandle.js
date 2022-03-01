1341edc196cc47187850f9eae8ff10c1642ef085
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-02 00:04:11 +0600

    input done

--- src/components/FormHandle/FormHandle.js
+++ src/components/FormHandle/FormHandle.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 
 const FormHandle = () => {
+    const[name,setName]=useState('');
+    const[password,setPassword]=useState('');
+    const[email,setEmail]=useState('')
+
+    const handleName=(e)=>{
+       const nameValue = e.target.value;
+       //console.log(nameValue)
+       setName(nameValue);
+    }
+    const handlePassword=(e)=>{
+        setPassword(e.target.value)
+    }
+    const handleEmail=(e)=>{
+        setEmail(e.target.value)
+    }
+    const handleButton=()=>{
+        console.log("clicked")
+
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+        <Form>
+          <label>Name: 
+              <input name="name" type="text" value={name} onChange={(e)=>handleName(e)}></input>
+          </label>
+          <br/>
+          <label>Password: 
+              <input name="password" type="text" value={password} onChange={(e)=>handlePassword(e)}></input>
+          </label>
+          <br/>
+          <label> Email
+              <input name="password" type="email" value={email} onChange={(e)=>handleEmail(e)}></input>
+          </label>
+          <br/>
+          <button onClick={()=>handleButton()}>Submit</button>
+        </Form>
+        </>
     );
 };
 
